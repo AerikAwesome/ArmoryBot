3a5760a53b24ba6ae22b61dd8ad988f63c9e871b
Erik Buter <e.t.buter@gmail.com>
2020-09-03 18:55:30 +0200

    Refactored planning message

--- ArmoryBot/Modules/PlanningModule.cs
+++ ArmoryBot/Modules/PlanningModule.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ArmoryBot.Extensions;
 using ArmoryBot.Models;
 using Disqord;
 using Disqord.Bot;
@@ -42,13 +43,19 @@ namespace ArmoryBot.Modules
             
             var items = itemString.Split(',', StringSplitOptions.RemoveEmptyEntries).Select(i => i.Trim()).ToList();
 
-            await ReplyAsync(
-                $"{planMessagePrefix} {items.Count()} items! Please respond with the appropriate reactions. (Yes/Maybe/No, and the days on which you are available, starting today)");
+            var mainMessage = await ReplyAsync($"{planMessagePrefix} {items.Count()} items! Please respond with the appropriate reactions. (Yes/Maybe/No, and the days on which you are available, starting today)");
+
+            var itemMessages = new List<RestUserMessage>();
+
             foreach (var item in items)
             {
-                var reply = await ReplyAsync(item);
-                await AddReactions(reply, DateTime.Today.DayOfWeek);
+                itemMessages.Add(await ReplyAsync(item));
+            }
 
+            await AddDayReactions(mainMessage, DateTime.Today.DayOfWeek);
+            foreach (var message in itemMessages)
+            {
+                await AddConfirmReactions(message);
             }
 
             await Context.Message.DeleteAsync();
@@ -65,18 +72,23 @@ namespace ArmoryBot.Modules
             }
             var items = itemString.Split(',', StringSplitOptions.RemoveEmptyEntries).Select(i => i.Trim()).ToList();
 
-            var planning = await GetPlanningResults();
-            if (planning.First().Message.CreatedAt < DateTimeOffset.Now.AddDays(-7))
+            var planning = await GetPlanningResult();
+            if (planning.Message.CreatedAt < DateTimeOffset.Now.AddDays(-7))
             {
                 await ReplyAsync("This planning was made more than a week ago, please create a new planning");
             }
-            var day = planning.First().DayUserNames.Keys.First();
 
+
+            var itemMessages = new List<RestUserMessage>();
 
             foreach (var item in items)
             {
-                var reply = await ReplyAsync(item);
-                await AddReactions(reply, day);
+                itemMessages.Add(await ReplyAsync(item));
+            }
+
+            foreach (var message in itemMessages)
+            {
+                await AddConfirmReactions(message);
             }
 
             await Context.Message.DeleteAsync();
@@ -86,7 +98,7 @@ namespace ArmoryBot.Modules
         [Description("Gets the results of the last planning")]
         public async Task ResultAsync()
         {
-            var planning = await GetPlanningResults();
+            var planning = await GetPlanningResult();
             var result = BuildResultMessage(planning);
             await ReplyAsync(result);
             
@@ -97,8 +109,8 @@ namespace ArmoryBot.Modules
         [Description("Gets the results of the last planning for today")]
         public async Task TodayAsync()
         {
-            var planning = await GetPlanningResults();
-            if (planning.First().Message.CreatedAt < DateTimeOffset.Now.AddDays(-7))
+            var planning = await GetPlanningResult();
+            if (planning.Message.CreatedAt < DateTimeOffset.Now.AddDays(-7))
             {
                 await ReplyAsync("This planning was made more than a week ago, please create a new planning");
             }
@@ -109,74 +121,83 @@ namespace ArmoryBot.Modules
             await Context.Message.DeleteAsync();
         }
         
-        private string BuildResultMessage(List<PlanningResult> results)
+        private string BuildResultMessage(PlanningResult result)
         {
             var sb = new StringBuilder("These are the results of the last planning:\n");
-            foreach (var result in results.Where(r => r.YesUserNames.Any() || r.MaybeUserNames.Any()).OrderBy(r => r.YesUserNames.Count))
+            foreach (var resultItem in result.Items.Where(r => r.YesUserNames.Any() || r.MaybeUserNames.Any()).OrderByDescending(r => r.YesUserNames.Count))
             {
-                sb.AppendLine($"**{result.Message.Content}**");
-                if (result.YesUserNames.Any())
-                    sb.AppendLine($"{YesReaction.MessageFormat} Wants to play: {string.Join(", ", result.YesUserNames)}");
-                if (result.MaybeUserNames.Any())
-                    sb.AppendLine($"{MaybeReaction.MessageFormat} Maybe wants to play: {string.Join(", ", result.MaybeUserNames)}");
-                if (result.NoUserNames.Any())
-                    sb.AppendLine($"{NoReaction.MessageFormat} Does not want to play: {string.Join(", ", result.NoUserNames)}");
+                sb.AppendLine($"**{resultItem.Message.Content}**");
+                if (resultItem.YesUserNames.Any())
+                    sb.AppendLine($"{YesReaction.MessageFormat} Wants to play: {string.Join(", ", resultItem.YesUserNames)}");
+                if (resultItem.MaybeUserNames.Any())
+                    sb.AppendLine($"{MaybeReaction.MessageFormat} Maybe wants to play: {string.Join(", ", resultItem.MaybeUserNames)}");
+                if (resultItem.NoUserNames.Any())
+                    sb.AppendLine($"{NoReaction.MessageFormat} Does not want to play: {string.Join(", ", resultItem.NoUserNames)}");
             }
 
             return sb.ToString();
         }
 
-        private string BuildTodayMessage(List<PlanningResult> results, DateTimeOffset date)
+        private string BuildTodayMessage(PlanningResult result, DateTimeOffset date)
         {
             var day = date.DayOfWeek;
             var sb = new StringBuilder($"These are the results for today, {DayReactions[day].MessageFormat}:\n");
-            foreach (var result in results.Where(r => r.DayUserNames[day].Any()).OrderBy(r => r.DayUserNames[day].Count))
+
+            sb.AppendLine("Available today:");
+            foreach (var userName in result.DayUserNames[day])
             {
-                sb.AppendLine($"**{result.Message.Content}**");
-                var names = result.DayUserNames[day];
-                sb.AppendLine($"Wants to play: {string.Join(", ", names)}");
+                sb.AppendLine($"- {userName}");
             }
 
+            sb.AppendLine("");
+            sb.AppendLine("Most voted items:");
+            foreach (var item in result.Items.Where(i => i.YesUserNames.Count > 0).OrderByDescending(i => i.YesUserNames.Count).Take(3))
+            {
+                sb.AppendLine($"**{item.Message.Content}** ({string.Join(", ", item.YesUserNames)})");
+            }
+            
             return sb.ToString();
         }
 
-        private async Task<List<PlanningResult>> GetPlanningResults()
+        private async Task<PlanningResult> GetPlanningResult()
         {
             var messages = await Context.Channel.GetMessagesAsync(250);
             var ownMessages = messages.Where(m => m.Author.Id == Context.Bot.CurrentUser.Id).ToList();
-            var planMessages = ownMessages.TakeWhile(m => !m.Content.StartsWith(planMessagePrefix)).Where(m => m.Reactions.Keys.Contains(YesReaction));
+            var planMessages = ownMessages.TakeUntilIncluding(m => !m.Content.StartsWith(planMessagePrefix));
 
-            var result = new List<PlanningResult>();
-            foreach (var message in planMessages.Reverse())
+            var result = new PlanningResult
+            {
+                Message = planMessages.Single(m => m.Content.StartsWith(planMessagePrefix))
+            };
+            
+            foreach (var dayReaction in result.Message.Reactions.Keys.Where(r => DayReactions.ContainsValue(r)))
+            {
+                var dictEntry = DayReactions.Single(e => e.Value.Equals(dayReaction));
+                var dayReactions = await result.Message.GetReactionsAsync(dayReaction);
+                result.DayUserNames.Add(dictEntry.Key, SelectUserName(dayReactions));
+            }
+
+            foreach (var message in planMessages.Where(m => m.Reactions.Keys.Contains(YesReaction)).Reverse())
             {
                 var messageResult = await GetPlanningResult(message);
-                result.Add(messageResult);
+                result.Items.Add(messageResult);
             }
 
             return result;
         }
-        private async Task<PlanningResult> GetPlanningResult(RestMessage message)
+        private async Task<ItemResult> GetPlanningResult(RestMessage message)
         {
             var yesReactions = await message.GetReactionsAsync(YesReaction);
             var maybeReactions = await message.GetReactionsAsync(MaybeReaction);
             var noReactions = await message.GetReactionsAsync(NoReaction);
 
-            var result = new PlanningResult
+            var result = new ItemResult()
             {
                 Message = message,
                 YesUserNames = SelectUserName(yesReactions),
                 MaybeUserNames = SelectUserName(maybeReactions),
                 NoUserNames = SelectUserName(noReactions),
-                DayUserNames = new Dictionary<DayOfWeek, List<string>>()
             };
-
-            foreach (var dayReaction in message.Reactions.Keys.Where(r => DayReactions.ContainsValue(r)))
-            {
-                var dictEntry = DayReactions.Single(e => e.Value.Equals(dayReaction));
-                var dayReactions = await message.GetReactionsAsync(dayReaction);
-                result.DayUserNames.Add(dictEntry.Key, SelectUserName(dayReactions));
-            }
-
             return result;
         }
 
@@ -185,17 +206,22 @@ namespace ArmoryBot.Modules
             return users.Where(r => !r.IsBot).Select(r => r.Name).ToList();
         }
 
-        private async Task AddReactions(RestUserMessage reply, DayOfWeek startDay)
+        private async Task AddDayReactions(RestUserMessage reply, DayOfWeek startDay)
         {
-            var reactions = new List<IEmoji> {YesReaction, MaybeReaction, NoReaction};
-            reactions.AddRange(GetDayReactions(startDay));
-
+            foreach (var reaction in GetDayReactions(startDay))
+            {
+                await reply.AddReactionAsync(reaction);
+            }
+        }
+        private async Task AddConfirmReactions(RestUserMessage reply)
+        {
+            var reactions = new List<IEmoji> { YesReaction, MaybeReaction, NoReaction };
             foreach (var reaction in reactions)
             {
                 await reply.AddReactionAsync(reaction);
             }
         }
-        
+
         private IEnumerable<IEmoji> GetDayReactions(DayOfWeek currentDayOfWeek)
         {
             var result = DayReactions
